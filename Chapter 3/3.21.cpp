823ffcc9f6ed1431207441b07ba6e31ded43fb26
u1017574 <u1017574@GRPC-05.scl.utah.edu>
2017-08-22 19:31:38 -0600

    Removed Comments
    Signed-off-by: u1017574 <u1017574@GRPC-05.scl.utah.edu>

--- Chapter 3/3.21.cpp
+++ Chapter 3/3.21.cpp
@@ -43,30 +43,30 @@ void PrintSizeAndContentsStringVector(const vector<string> v)
 	cout << "\n";
 }
 
-//int main()
-//{
-//	vector<int> v1;
-//	PrintSizeAndContents(v1);
-//
-//	vector<int> v2(10);
-//	PrintSizeAndContents(v2);
-//
-//	vector<int> v3(10, 42);
-//	PrintSizeAndContents(v3);
-//
-//	vector<int> v4{ 10 };
-//	PrintSizeAndContents(v4);
-//
-//	vector<int> v5{ 10,42 };
-//	PrintSizeAndContents(v5);
-//
-//	vector<string> v6{ 10 };
-//	PrintSizeAndContents(v6);
-//
-//	vector<string> v7 {10, "hi"};
-//	PrintSizeAndContents(v7);
-//
-//	cin.get();
-//
-//	return 0;
-//}
+int main()
+{
+	vector<int> v1;
+	PrintSizeAndContents(v1);
+
+	vector<int> v2(10);
+	PrintSizeAndContents(v2);
+
+	vector<int> v3(10, 42);
+	PrintSizeAndContents(v3);
+
+	vector<int> v4{ 10 };
+	PrintSizeAndContents(v4);
+
+	vector<int> v5{ 10,42 };
+	PrintSizeAndContents(v5);
+
+	vector<string> v6{ 10 };
+	PrintSizeAndContents(v6);
+
+	vector<string> v7 {10, "hi"};
+	PrintSizeAndContents(v7);
+
+	cin.get();
+
+	return 0;
+}
